edd6b11df4a09ef0f449a6655f0bd05d0b5f33e0
mayuroks <mayurzenith@gmail.com>
2019-01-23 15:03:59 +0530

    questioncard UI changes

--- src/components/questionCard.js
+++ src/components/questionCard.js
@@ -4,14 +4,34 @@ import { View, Text } from 'react-native';
 
 // make component
 const QuestionCard = (props) => {
+    const { containerStyle, questionStyle, optionStyle } = styles;
+
     return(
-        <View>
-            <Text>{props.question}</Text>
-            <Text>{props.option1}</Text>
-            <Text>{props.option2}</Text>
+        <View style={containerStyle}>
+            <Text style={questionStyle}>{props.question}</Text>
+            <Text style={optionStyle}>{props.option1}</Text>
+            <Text style={optionStyle}>{props.option2}</Text>
         </View>
     );
 }
 
 // export the component
 export default QuestionCard;
+
+const styles = {
+    containerStyle: {
+        marginTop: 64,
+        marginLeft: 32,
+        marginRight: 32
+    },
+    questionStyle: {
+        fontSize: 28,
+        marginTop: 12,
+        marginBottom: 16
+    },
+    optionStyle: {
+        fontSize: 18,
+        marginTop: 8,
+        marginBottom: 8
+    }
+}
