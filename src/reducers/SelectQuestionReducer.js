d2b6ffbbff57eba31020cb2e9a820253799a228a
mayuroks <mayurzenith@gmail.com>
2019-01-28 23:16:13 +0530

    BUG FIX  question reducer giving wrong questionId

--- src/reducers/SelectQuestionReducer.js
+++ src/reducers/SelectQuestionReducer.js
@@ -1,7 +1,7 @@
 import data from '../questions.json';
 
 const INITIAL_STATE = {
-    questionId: 0,
+    questionId: -1,
     question: null,
     questionCount: 0
 };
@@ -9,10 +9,11 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case 'next_question':
+            const questionId = state.questionId + 1;
             return {
                 ...state,
-                questionId: state.questionId + 1, // FIXME give wrong question number
-                question: data[state.questionId],
+                questionId: questionId,
+                question: data[questionId],
                 questionCount: data.length
             };
         default:
