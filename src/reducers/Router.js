67bf2c1561bf374f54f838d1acc57af763d5887b
mayuroks <mayurzenith@gmail.com>
2019-02-05 22:59:57 +0530

    minor fixes

--- src/reducers/Router.js
+++ src/reducers/Router.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import Home from '../components/Home';
 import Splash from '../components/Splash';
-// import FBLoginButton from '../components/Login';
 // FIXME remove backstack
 
 const RouterComponent = () => {
     return (
         <Router>
             <Scene key="root">
-                {/* <Scene key="splash" component={Splash} title="Splash" hideNavBar={true} initial /> */}
+                <Scene key="splash" component={Splash} title="Splash" hideNavBar={true} initial />
                 <Scene key="home" component={Home} title="Home" hideNavBar={true} />
             </Scene>
         </Router>
