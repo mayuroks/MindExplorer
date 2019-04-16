bf4092ef89062693f0185e5011d1d7e779bff6dd
Mayur Rokade <mayur@grexit.com>
2019-04-16 22:23:24 +0530

    removed Pods folder and updated gitignore

--- ios/Pods/Target Support Files/Pods-MindExplorer/Pods-MindExplorer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MindExplorer : NSObject
-@end
-@implementation PodsDummy_Pods_MindExplorer
-@end
--- ios/Pods/Target Support Files/Pods-MindExplorerTests/Pods-MindExplorerTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MindExplorerTests : NSObject
-@end
-@implementation PodsDummy_Pods_MindExplorerTests
-@end
